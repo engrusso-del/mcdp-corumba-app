30e0a4e7e9107d9673a393a75c7d64b5a575f7b8
CasadoPai <engrusso@gmail.com>
2026-02-05 13:45:05 -0400

    Add files via upload

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
